8d1303b7a90e93766d8d8aaade828cb6954fa988
davmmeyer <davmmeyer@gmail.com>
2017-10-17 19:27:17 -0700

    Added first draft of the landing page. Includes modal for new game and clickable link to open static game.

--- cos/views/views.py
+++ cos/views/views.py
@@ -1,10 +1,14 @@
 from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='templates/game.jinja2')
+@view_config(route_name='game', renderer='templates/game.jinja2')
 def my_view(request):
     return {'project': 'Catan Board'}
 
+
+@view_config(route_name='landingpage', renderer='templates/landingpage.jinja2')
+def my_view2(request):
+    return {'project': 'Settlers of Catan Game'}
 
 # @view_config(route_name='generate_ajax_data', renderer='json')
 # def my_ajax_view(request):
