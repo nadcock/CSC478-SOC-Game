e87a491a07c417b7a5dc3742e0dbd41648feac9e
nickadcock <nick@nicksemail.com>
2017-10-20 18:24:35 -0700

    Work for #28
    Refactored some code to be more Pythonic
    Added docstrings for documentation purposes

--- cos/models/Game.py
+++ cos/models/Game.py
@@ -1,21 +1,20 @@
 import random
 from Player import Player
 
+
 class Game(object):
     """ Class for defining a game Object 
     
         Current Attributes
         ------------------
-        __game_id: String
-        __players: [Player]
+        id: String
+        players: [Player]
         
         Methods
         -------
-        getGameId()
-        getFirstPlayer()
-        getPlayerCount()
-        getIsGameFull()
-        addPlayer(String)
+        get_player_count()
+        is_game_full()
+        add_player(String)
     
     """
 
@@ -28,34 +27,22 @@ class Game(object):
                 - sets private attributes of game_id (random string of alpaha-numeric chars and
                   creates first player object named "Player1" 
         """
-        id = ''.join(random.choice('0123456789ABCDEFGHIJKLMNZQRSTUVWXYZ') for i in range(6))
-        self.__game_id = id
-        self.__players = [Player(name="Player 1")]
+        id_string = ''.join(random.choice('0123456789ABCDEFGHIJKLMNZQRSTUVWXYZ') for i in range(6))
+        self.id = id_string
+        self.players = [Player(name="Player 1")]
 
-    def getGameId(self):
-        """ returns game_id set by constructor """
-        return self.__game_id
-
-    def getPlayer(self, player_number):
-        """ returns Player at selected number """
-        return self.__players[player_number - 1]
-
-    def getPlayers(self):
-        """ returns list of all Player objects in game """
-        return self.__players
-
-    def getPlayerCount(self):
+    def get_player_count(self):
         """ returns number of players currently added to game """
-        return len(self.__players)
+        return len(self.players)
 
-    def getIsGameFull(self):
+    def is_game_full(self):
         """ returns True if game has reached the maximum, otherwise returns False """
-        if self.getPlayerCount() < self.PLAYER_MAX:
+        if self.get_player_count() < self.PLAYER_MAX:
             return False
         else:
             return True
 
-    def addPlayer(self, name):
+    def add_player(self, name):
         """
             If the players maximum has not been reached, a Player object is created and added to
             the list of players, then that player object is returned. Otherwise, None is returned. 
@@ -67,28 +54,37 @@ class Game(object):
 
             Returns
             -------
-            Player
-                Player object, or if max has been reached, None
+            Player (Player object, or if max has been reached, None)
 
         """
-        if self.__players.count == self.PLAYER_MAX:
+        if self.get_player_count() == self.PLAYER_MAX:
             print "Player maximum has already been reached."
             return None
         else:
-            new_player = Player(name="Player " + str(self.getPlayerCount() + 1))
-            self.__players.append(new_player)
+            new_player = Player(name="Player " + str(self.get_player_count() + 1))
+            self.players.append(new_player)
             return new_player
 
 
 class Games(object):
     """ Class for keeping track of all games on the server. This is registered as a global attribute with the
-        registry and can be accessed using (in views) request.registry.games or (in config) config.registry.games 
+        registry and can be accessed using (in views) request.registry.games or (in config) config.registry.games
+         
+        Current Attributes
+        ------------------
+        games: Dictionary {game_id: Game}
+        
+        Methods
+        -------
+        addGame(Game)
+        getGamesByID(String)
     """
+
     def __init__(self):
         """ Initializes the games dictionary with an empty dictionary object """
-        self.__games = {}
+        self.games = {}
 
-    def addGame(self, game):
+    def add_game(self, game):
         """
             Adds a game to global games dictionary in the form of {game_id: Game} 
 
@@ -102,9 +98,9 @@ class Games(object):
             None
 
         """
-        self.__games[game.getGameId()] = game
+        self.games[game.id] = game
 
-    def getGameByID(self, game_id):
+    def get_game_by_id(self, game_id):
         """
             Returns a game object based on provided game_id 
 
@@ -118,13 +114,13 @@ class Games(object):
             None
 
         """
-        return self.__games[game_id]
+        return self.games[game_id]
 
-    def printGames(self):
+    def print_games(self):
         """ Prints the current list of games to the console. Used for debugging """
         print "Current Games List:"
-        for id in self.__games:
-            print "'Game ID': " + id + "   'First Player': " + self.__games[id].getPlayer(1).getPlayerId()
+        for id in self.games:
+            print "'Game ID': " + id + "   'First Player': " + self.games[id].players[0].id
         print ""
 
 
--- cos/models/Player.py
+++ cos/models/Player.py
@@ -1,15 +1,21 @@
 import random
 
+
 class Player(object):
+    """ Class for defining a Player Object 
+
+            Current Attributes
+            ------------------
+            id: String
+            name: String
+
+            Methods
+            -------
+            None
+
+        """
     def __init__(self, name):
-        id = ''.join(random.choice('0123456789ABCDEFGHIJKLMNZQRSTUVWXYZ') for i in range(6))
-        self.__player_id = id
-        self.__player_name = name
+        id_string = ''.join(random.choice('0123456789ABCDEFGHIJKLMNZQRSTUVWXYZ') for i in range(6))
+        self.id = id_string
+        self.name = name
 
-    def getPlayerId(self):
-        """ returns player_id set by constructor """
-        return self.__player_id
-
-    def getPlayerName(self):
-        """ returns player_name set by constructor """
-        return self.__player_name
