281cdb47c351f854bf7eda21665a84ffe0f2ca21
Nick Adcock <dev@nicksemail.com>
2017-10-20 17:16:44 -0700

    New game#26 (#37)
    * Work for #26
    
    Created Models:
       - Game
       - Player
    
    Created js script file for landing page:
       - landing_page_api_calls.js
    
    Script file includes an ajax call to create a new game and receives a game ID and player ID in response. It then redirects to page with unique game.
    
    * Continued Work for #26
    
    Added a semi-persistant state for game objects so that multiple games can exist at the same time.
    
    NOTE: THESE GAMES ONLY PERSIST WHILE THE SERVER IS RUNNING. IF YOU RESTART THE SERVER ALL GAMES WILL BE LOST.
    
    * Continued Work for #26
    
    Just some formatting changes
    
    * Continued Work for #26
    
    Added support for Response so JQuery can autoparse the returned JSON
    
    * Continued Work for #26
    
    Changed JQuery call from GET to POST to align with best practices

--- cos/views/views.py
+++ cos/views/views.py
@@ -11,6 +11,24 @@ import json
 def my_view(request):
     return {'project': 'Catan Board'}
 
+@view_config(route_name='game', renderer='templates/game.jinja2')
+def game_view(request):
+    """ Returns Game Play page based on ID"""
+    return {'project': 'Catan Board: Game_ID = ' + request.matchdict['game_id']}
+
+@view_config(route_name='createGame', renderer='json')
+def create_game_view(request):
+    new_game = Game()
+    request.registry.games.addGame(new_game)
+    request.registry.games.printGames()
+    return_data = {'game': {'game_id': new_game.getGameId(),
+                   'player_id': new_game.getFirstPlayer().getPlayerId()}}
+    json_return = json.dumps(return_data)
+    return Response(
+        content_type='json',
+        body=json_return
+    )
+
 
 @view_config(route_name='game', renderer='templates/game.jinja2')
 def game_view(request):
