7844755f911f0a053d37f2b9d82647dad04cd97a
nickadcock <nick@nicksemail.com>
2017-10-29 16:48:51 -0700

    Created api calls for:   - Starting Game   - Completing a turn   - Waiting for turn

--- cos/views/views.py
+++ cos/views/views.py
@@ -1,6 +1,7 @@
 from pyramid.view import view_config
 from pyramid.response import Response
 from cos.models.Game import Game
+import time
 import json
 
 
@@ -320,7 +321,7 @@ def roll_dice(request):
                                         "dice_total": "5",
                                         "dice_two": "3"
                                     }
-        """
+    """
     json_body = request.json_body
     if 'game_id' in json_body:
         game_id = json_body['game_id']
@@ -356,6 +357,141 @@ def roll_dice(request):
         body=json_return
     )
 
-# @view_config(route_name='generate_ajax_data', renderer='json')
-# def my_ajax_view(request):
-#     return {'message': 'Hello World'}
+@view_config(route_name='startGame', renderer='json')
+def start_game(request):
+    """ Starts game by setting a turn iterator in game object
+
+            Parameters
+            ----------
+            request: Request 
+                - required JSON parameters: "game_id": String
+
+            Returns
+            -------
+
+            Json object containing: {"Success": Bool}
+    """
+    json_body = request.json_body
+    if 'game_id' in json_body:
+        game_id = json_body['game_id']
+    else:
+        request.response.status = 400
+        return {'error': "'game_id' is a required parameter for this request"}
+
+    if game_id not in request.registry.games.games:
+        request.response.status = 400
+        return {'error': "Requested Game with id '%s' does not exist." % game_id}
+    game = request.registry.games.games[game_id]
+
+    if len(game.turn_order) < 3:
+        request.response.status = 400
+        return {'error': "You cannot start a game with less than 3 players"}
+
+    if not game.game_started:
+        game.start_game()
+
+    return_data = {"Success": "True"}
+    json_return = json.dumps(return_data)
+    return Response(
+        content_type='json',
+        body=json_return
+    )
+
+@view_config(route_name='completeTurn', renderer='json')
+def complete_turn(request):
+    """ Completes requested player's turn and sets next turn to the next player in iterator.
+        Returns ploayer_id of next player in iterator
+
+            Parameters
+            ----------
+            request: Request 
+                - required JSON parameters: "game_id": String, "player_id": String
+
+            Returns
+            -------
+
+            Json object containing: {"Success": Bool,
+                                     "New_current_player": String}
+    """
+    json_body = request.json_body
+    if 'game_id' in json_body:
+        game_id = json_body['game_id']
+    else:
+        request.response.status = 400
+        return {'error': "'game_id' is a required parameter for this request"}
+
+    if game_id not in request.registry.games.games:
+        request.response.status = 400
+        return {'error': "Requested Game with id '%s' does not exist." % game_id}
+    game = request.registry.games.games[game_id]
+
+    if 'player_id' in json_body:
+        player_id = json_body['player_id']
+    else:
+        request.response.status = 400
+        return {'error': "'player_id' is a required parameter for this request"}
+
+    if player_id not in game.players:
+        request.response.status = 400
+        return {'error': "Requested Player with id '%s' does not exist in this Game." % player_id}
+
+    if player_id != game.current_player_id:
+        request.response.status = 400
+        return {'error': "Requested Player with id '%s' does not have current turn." % player_id}
+
+    game.take_turn()
+
+    return_data = {"Success": "True",
+                   "New_current_player": game.current_player_id}
+    json_return = json.dumps(return_data)
+    return Response(
+        content_type='json',
+        body=json_return
+    )
+
+@view_config(route_name='waitForTurn', renderer='json')
+def wait_for_turn(request):
+    """ This checks if it is the requested player's turn and if so returns True, otherwise it sleeps until
+        it is the players turn, at which point it returns True.
+
+            Parameters
+            ----------
+            request: Request 
+                - required JSON parameters: "game_id": String, "player_id": String
+
+            Returns
+            -------
+
+            Json object containing: {"MyTurn": Bool}
+    """
+    json_body = request.json_body
+    if 'game_id' in json_body:
+        game_id = json_body['game_id']
+    else:
+        request.response.status = 400
+        return {'error': "'game_id' is a required parameter for this request"}
+
+    if game_id not in request.registry.games.games:
+        request.response.status = 400
+        return {'error': "Requested Game with id '%s' does not exist." % game_id}
+    game = request.registry.games.games[game_id]
+
+    if 'player_id' in json_body:
+        player_id = json_body['player_id']
+    else:
+        request.response.status = 400
+        return {'error': "'player_id' is a required parameter for this request"}
+
+    if player_id not in game.players:
+        request.response.status = 400
+        return {'error': "Requested Player with id '%s' does not exist in this Game." % player_id}
+
+    while player_id != game.current_player_id:
+        time.sleep(5)
+
+    return_data = {"MyTurn": "True"}
+    json_return = json.dumps(return_data)
+    return Response(
+        content_type='json',
+        body=json_return
+    )
