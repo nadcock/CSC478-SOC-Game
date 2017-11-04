2f0cb74b1c3a9412ef6894257fc4f4f2a5be82c4
davmmeyer <davmmeyer@gmail.com>
2017-11-04 08:59:54 -0700

    Added function to get dice roll. Added UI to represent dice roll.

--- cos/static/gamedice.js
+++ cos/static/gamedice.js
@@ -4,58 +4,6 @@
 
 
 /**
- * This function renders two dice on the game board
- *
- * The render will appear as two separate elements initialized to '0'
- *  --------------
- * |      |       |
- * |  5   |   3   |
- * |      |       |
- *  --------------
- *   <Roll Dice>
- */
-function display_dice() {
-
-    // Generate dice structure
-    var body = document.getElementById("gamedice");
-
-
-
-    var diceTable = document.createElement("table");
-    var row = document.createElement("tr");
-
-    // Create first dice cell
-    var cell1 = document.createElement("td");
-    var cell1div = document.createElement("div");
-    var cellh3 = document.createElement("h3");
-    var span1 = document.createElement("span");
-    var cellh3Text1 = document.createTextNode("0");
-    var cellspanText1 = document.createTextNode("dice one");
-
-
-
-/*    // Append first cell
-    cell1.appendChild(cellText1);
-    row.appendChild(cell1);
-
-    // Create second dice cell
-    var cell2 = document.createElement("td");
-    var cellText2 = document.createTextNode("0");
-
-    // Append second cell
-    cell2.appendChild(cellText2);
-    row.appendChild(cell2);
-
-    diceTableBody.appendChild(row);
-    diceTable.appendChild(diceTableBody);
-    body.appendChild(diceTable);
-
-    // Styling
-    diceTable.setAttribute("border", "2");*/
-}
-
-
-/**
  * Landing Page modal: this function checks if start game modal form has a game name. If it has,
  * then the game name is submitted and a new game is started:
  * 1) Game name
