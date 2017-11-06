ee804db6203eba0ae48df841cac3ce4a970e2a11
mpolsgrove <32312121+mpolsgrove@users.noreply.github.com>
2017-11-06 15:56:51 -0600

    Update game_page_api_calls.js
    Added buy_settlement AJAX call, which sends information about newly placed settlements to backend

--- cos/static/game_page_api_calls.js
+++ cos/static/game_page_api_calls.js
@@ -4,10 +4,23 @@ function get_player_info (cb_func,stage,layer) {
         url: '/api/game/getPlayersInGame',
         type: 'POST',
         dataType: 'json',
-        data: JSON.stringify({"game_id": "GAME1"}),
+        data: JSON.stringify({"game_id": document.getElementById("game_id").innerHTML}),
         contentType: "application/json",
         success: function (data) {
             cb_func(data, stage, layer);
+        }
+    })
+}
+//Posts information about newly constructed settlements to backend
+function buy_settlement (player_ID, settlement_ID) {
+    $.ajax({
+        url: '/api/player/buySettlement',
+        type: 'POST',
+        dataType: 'json',
+        data: JSON.stringify({"game_id": document.getElementById("game_id").innerHTML, "player_id": player_ID, "settlement_id": settlement_ID}),
+        contentType: "application/json",
+        success: function () {
+            console.log("buy_settlement");
         }
     })
 }
