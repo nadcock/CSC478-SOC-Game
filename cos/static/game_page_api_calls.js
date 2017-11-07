255a197e31dcf4a1faa614341f4d0ca46a7933ac
mpolsgrove <32312121+mpolsgrove@users.noreply.github.com>
2017-11-07 16:13:30 -0600

    Update game_page_api_calls.js
    Updated buy_settlement function with callback function for settlement UI update

--- cos/static/game_page_api_calls.js
+++ cos/static/game_page_api_calls.js
@@ -12,7 +12,7 @@ function get_player_info (cb_func,stage,layer) {
     })
 }
 //Posts information about newly constructed settlements to backend
-function buy_settlement (player_ID, settlement_ID) {
+function buy_settlement (player_ID, settlement_ID, x, y, settlementX, settlementY, stage, layer, cb_func) {
     $.ajax({
         url: '/api/player/buySettlement',
         type: 'POST',
@@ -20,7 +20,7 @@ function buy_settlement (player_ID, settlement_ID) {
         data: JSON.stringify({"game_id": document.getElementById("game_id").innerHTML, "player_id": player_ID, "settlement_id": settlement_ID}),
         contentType: "application/json",
         success: function () {
-            console.log("buy_settlement");
+            cb_func(x, y, settlementX, settlementY, stage, layer);
         }
     })
 }
