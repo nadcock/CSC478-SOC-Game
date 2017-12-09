38215b7f226328e80fc9710808b96a4a296282c7
mpolsgrove <matthewpolsgrove@gmail.com>
2017-12-09 16:42:45 -0600

    Added comments to landing_page_api_calls.js

--- cos/static/landing_page_api_calls.js
+++ cos/static/landing_page_api_calls.js
@@ -1,6 +1,17 @@
 /**
  * Created by nickadcock on 10/18/17.
  */
+
+/**
+ * This function allows a user to create a game from the landing page
+ * @param gameName
+ *
+ * This function implements the following requirements:
+ * 3.1.2
+ * 3.1.3
+ * 3.1.3.1.1
+ * 3.1.3.1.2
+ */
 function create_game(gameName) {
     $.ajax({
         url     :   '/api/game/createGame',
